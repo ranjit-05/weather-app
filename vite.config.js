9d48fc2475dab0a213558c32bf2648541e3fc540
ranjit-05 <ranjitsalunkhe005@gmail.com>
2026-03-13 18:55:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/weather-app/',   // must match your repo name exactly
+  base: '/weather-app/',  
 })
